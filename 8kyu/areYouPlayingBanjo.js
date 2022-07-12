d5af6b0321d40dcd712506141ac811354ee31806
Mecca Young <102000967+CodeMomx@users.noreply.github.com>
2022-07-12 09:32:45 -0400

    Delete areYouPlayingBanjo.js

--- 8kyu/areYouPlayingBanjo.js
+++
@@ -1,9 +0,0 @@
-//Create a function which answers the question "Are you playing banjo?".
-If your name starts with the letter "R" or lower case "r", you are playing banjo!
-function areYouPlayingBanjo(name) {
-  if (name[0] === 'R' || name[0] === 'r'){
-     return name + ' plays banjo'
-   }else{
-   return name + ' does not play banjo'
-     }
-}
